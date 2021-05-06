eb88546849657a57687d7b1012f3c4e6d8241b0e
camilapessoa <camilabentespessoa@gmail.com>
2021-05-06 04:40:50 -0300

    add helper

---
+++ src/helpers/helper.js
@@ -0,0 +1,16 @@
+/* const express = require('express')
+
+/* let newDate = new Date().toTimeString().newDate.split().reverse().toString()
+
+let intervalsHelper = new Date()
+intervalsHelper = intervalsHelper.geUTCFHours().getMinutes() */
+
+//const path = require("../dev-data/schedules.json")
+
+
+
+/* module.exports = {
+  newDate,
+  intervalsHelper
+
+}  */
